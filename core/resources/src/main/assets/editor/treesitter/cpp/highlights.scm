0fd1766bbfd169e6a4ddc763710ae523bada8d4c
android-zeros <16684848382@163.com>
2026-03-08 02:42:34 +0800

    update

--- core/resources/src/main/assets/editor/treesitter/cpp/highlights.scm
+++ core/resources/src/main/assets/editor/treesitter/cpp/highlights.scm
@@ -1,77 +1,131 @@
+; --------------------------------------
+; Preprocessor
+; --------------------------------------
+(preproc_directive) @keyword.directive
+(preproc_def name: (identifier) @constant.macro)
+(preproc_function_def name: (identifier) @function.macro)
+
+(preproc_include
+  path: (string_literal) @string.special)
+(preproc_include
+  path: (system_lib_string) @string.special.system)
+
+; --------------------------------------
+; Namespaces & Scopes
+; --------------------------------------
+(namespace_identifier) @namespace
+
+; --------------------------------------
+; Types
+; --------------------------------------
+(primitive_type) @type.builtin
+(type_identifier) @type
+(auto) @type.builtin
+
+(struct_specifier name: (type_identifier) @type)
+(class_specifier name: (type_identifier) @type)
+(enum_specifier name: (type_identifier) @type)
+(union_specifier name: (type_identifier) @type)
+
+; --------------------------------------
 ; Functions
-
-(call_expression
-  function: (qualified_identifier
-    name: (identifier) @function))
-
-(template_function
-  name: (identifier) @function)
-
-(template_method
-  name: (field_identifier) @function)
-
-(template_function
-  name: (identifier) @function)
+; --------------------------------------
+(function_declarator
+  declarator: (identifier) @function)
 
 (function_declarator
   declarator: (qualified_identifier
     name: (identifier) @function))
 
 (function_declarator
-  declarator: (field_identifier) @function)
+  declarator: (field_identifier) @function.method)
 
-; Types
+(call_expression
+  function: (identifier) @function.call)
 
-((namespace_identifier) @type
- (#match? @type "^[A-Z]"))
+(call_expression
+  function: (qualified_identifier
+    name: (identifier) @function.call))
 
-(auto) @type
+(call_expression
+  function: (field_expression
+    field: (field_identifier) @function.method.call))
 
-; Constants
+(template_function name: (identifier) @function)
 
+; --------------------------------------
+; Variables & Fields
+; --------------------------------------
+(field_identifier) @variable.field
 (this) @variable.builtin
-(null "nullptr" @constant)
 
-; Modules
-(module_name
-  (identifier) @module)
+(parameter_declaration
+  declarator: (identifier) @variable.parameter)
 
+(parameter_declaration
+  declarator: (reference_declarator (identifier) @variable.parameter))
+
+(parameter_declaration
+  declarator: (pointer_declarator (identifier) @variable.parameter))
+
+(identifier) @variable
+
+; --------------------------------------
+; Literals & Constants
+; --------------------------------------
+(number_literal) @number
+(string_literal) @string
+(raw_string_literal) @string
+(char_literal) @string
+(escape_sequence) @string.escape
+(comment) @comment
+
+(null) @constant.builtin
+(true) @constant.builtin
+(false) @constant.builtin
+
+; --------------------------------------
 ; Keywords
+; --------------------------------------
+[
+  "if" "else" "for" "while" "do" "break" "continue" "return" 
+  "switch" "case" "default" "goto"
+] @keyword.control[
+  "const" "static" "extern" "volatile" "mutable" "register" 
+  "inline" "virtual" "explicit" "friend" "constexpr" 
+  "consteval" "constinit" "unsigned" "signed"
+] @keyword.modifier[
+  "public" "protected" "private"
+] @keyword.visibility[
+  "class" "struct" "union" "enum" "template" "typename" 
+  "namespace" "using" "typedef" "concept" "requires" 
+  "new" "delete" "try" "catch" "throw" "noexcept" 
+  "sizeof" "decltype" "alignof" "alignas" "typeid" "operator"
+  "co_await" "co_return" "co_yield" "import" "export" "module"
+] @keyword[
+  "static_cast" "dynamic_cast" "reinterpret_cast" "const_cast"
+] @keyword.cast
+
+; --------------------------------------
+; Operators & Punctuation
+; --------------------------------------
+[
+  "+" "-" "*" "/" "%" "++" "--"
+  "==" "!=" "<" ">" "<=" ">="
+  "&&" "||" "!"
+  "&" "|" "^" "~" "<<" ">>"
+  "=" "+=" "-=" "*=" "/=" "%=" "<<=" ">>=" "&=" "|=" "^="
+  "?" ":"
+] @operator
 
 [
- "catch"
- "class"
- "co_await"
- "co_return"
- "co_yield"
- "constexpr"
- "constinit"
- "consteval"
- "delete"
- "explicit"
- "final"
- "friend"
- "mutable"
- "namespace"
- "noexcept"
- "new"
- "override"
- "private"
- "protected"
- "public"
- "template"
- "throw"
- "try"
- "typename"
- "using"
- "concept"
- "requires"
- "virtual"
- "import"
- "export"
- "module"
-] @keyword
+  "::" "->" "."
+] @punctuation.special
 
-; Strings
+[
+  "(" ")" "[" "]" "{" "}"
+] @punctuation.bracket
 
-(raw_string_literal) @string
+[
+  "," ";"
+] @punctuation.delimiter
