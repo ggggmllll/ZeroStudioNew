5ec0cd2a49d06ccf8d0011e0c557aac19913f501
android-zeros <16684848382@163.com>
2026-04-01 01:13:52 +0800

    fix SwitchPreference bug

--- utilities/preferences/src/main/java/com/itsaky/androidide/preferences/SwitchPreference.kt
+++ utilities/preferences/src/main/java/com/itsaky/androidide/preferences/SwitchPreference.kt
@@ -15,24 +15,15 @@
  *   along with AndroidIDE.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-/*
- *  This file is part of AndroidIDE.
- *
- *  AndroidIDE is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- */
-
 package com.itsaky.androidide.preferences
 
 import android.content.Context
 import androidx.preference.Preference
-import androidx.preference.SwitchPreference as AndroidXSwitchPreference
+import androidx.preference.TwoStatePreference
 import kotlin.reflect.KMutableProperty0
 
 /**
- * A switch preference.
+ * A switch preference 
  *
  * @author Akash Yadav
  * @author android_zero
@@ -45,11 +36,11 @@ constructor(val setValue: ((Boolean) -> Unit)? = null, val getValue: (() -> Bool
   constructor(property: KMutableProperty0<Boolean>) : this(property::set, property::get)
 
   override fun onCreatePreference(context: Context): Preference {
-    val pref = AndroidXSwitchPreference(context)
-    
-    pref.isChecked = prefValue()
-    
-    return pref
+    return androidx.preference.SwitchPreference(context).apply {
+        isPersistent = false
+        
+        isChecked = getValue?.invoke() ?: false
+    }
   }
 
   override fun onPreferenceChanged(preference: Preference, newValue: Any?): Boolean {
@@ -57,14 +48,18 @@ constructor(val setValue: ((Boolean) -> Unit)? = null, val getValue: (() -> Bool
     
     setValue?.invoke(isChecked)
     
-    if (preference is AndroidXSwitchPreference) {
+    if (preference is TwoStatePreference) {
         preference.isChecked = isChecked
     }
     
     return true
   }
 
-  private fun prefValue(): Boolean {
-    return getValue?.invoke() ?: false
+  override fun onCreateView(context: Context): Preference {
+    return super.onCreateView(context).apply {
+      if (this is TwoStatePreference) {
+        this.isChecked = getValue?.invoke() ?: false
+      }
+    }
   }
 }
