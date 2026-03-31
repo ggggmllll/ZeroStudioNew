9d897eb5d28eec8cfc50aa6fafc847169819603b
android-zeros <16684848382@163.com>
2026-03-31 22:13:23 +0800

    fix SwitchPreference bug

--- utilities/preferences/src/main/java/com/itsaky/androidide/preferences/SwitchPreference.kt
+++ utilities/preferences/src/main/java/com/itsaky/androidide/preferences/SwitchPreference.kt
@@ -15,36 +15,56 @@
  *   along with AndroidIDE.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+/*
+ *  This file is part of AndroidIDE.
+ *
+ *  AndroidIDE is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ */
+
 package com.itsaky.androidide.preferences
 
 import android.content.Context
 import androidx.preference.Preference
+import androidx.preference.SwitchPreference as AndroidXSwitchPreference
 import kotlin.reflect.KMutableProperty0
 
 /**
  * A switch preference.
  *
  * @author Akash Yadav
+ * @author android_zero
  */
 abstract class SwitchPreference
 @JvmOverloads
 constructor(val setValue: ((Boolean) -> Unit)? = null, val getValue: (() -> Boolean)? = null) :
   BasePreference() {
 
-    constructor(property: KMutableProperty0<Boolean>) : this(property::set, property::get)
+  constructor(property: KMutableProperty0<Boolean>) : this(property::set, property::get)
 
   override fun onCreatePreference(context: Context): Preference {
-    val pref = androidx.preference.SwitchPreference(context)
+    val pref = AndroidXSwitchPreference(context)
+    
     pref.isChecked = prefValue()
+    
     return pref
   }
 
   override fun onPreferenceChanged(preference: Preference, newValue: Any?): Boolean {
-    setValue?.let { it(newValue as Boolean? ?: prefValue()) }
+    val isChecked = newValue as? Boolean ?: false
+    
+    setValue?.invoke(isChecked)
+    
+    if (preference is AndroidXSwitchPreference) {
+        preference.isChecked = isChecked
+    }
+    
     return true
   }
 
   private fun prefValue(): Boolean {
-    return getValue?.let { it() } ?: false
+    return getValue?.invoke() ?: false
   }
 }
