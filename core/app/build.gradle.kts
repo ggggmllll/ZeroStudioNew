d8a061f2065c3c8cf0b5e06c9d9086843d8210a6
android-zeros <16684848382@163.com>
2026-04-11 05:52:10 +0800

    update

--- core/app/build.gradle.kts
+++ core/app/build.gradle.kts
@@ -126,7 +126,7 @@ configurations.all {
   // }
   
   exclude(group = "org.jetbrains.kotlin", module = "kotlin-android-extensions-runtime")
-  // exclude(group = "org.jetbrains.kotlin", module = "kotlin-compiler")
+  exclude(group = "org.jetbrains.kotlin", module = "kotlin-compiler")
   // exclude(group = "org.jetbrains.kotlin", module = "kotlin-compiler-embeddable")
   exclude(group = "com.google.firebase", module = "protolite-well-known-types")
   exclude(group = "com.google.protobuf", module = "protobuf-java")
